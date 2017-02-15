9089f97edbb5ab55986a6f99de0245375156d277
Surho <surho007@hotmail.com>
2017-02-16 00:08:54 +0300

    aa

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ closeCart.addEventListener("click", function(event) {
   cartPopup.classList.remove("cart-popup-show");
 });
 
+
 var map=document.querySelector(".information-map");
 var mapPopup=document.querySelector(".map-popup");
 var closeMap=mapPopup.querySelector(".popup-close");
@@ -26,15 +27,40 @@ closeMap.addEventListener ("click", function(event) {
   mapPopup.classList.remove("map-popup-show");
 });
 
+
 var contactUs=document.querySelector(".btn-getlost");
 var contactUsPopup=document.querySelector(".information-popup");
 var closeInformation=contactUsPopup.querySelector(".popup-close");
+var form=contactUsPopup.querySelector("form");
+var formName=contactUsPopup.querySelector("input[name=name]");
+var formEmail=contactUsPopup.querySelector("input[name=email]");
+var formMessage=contactUsPopup.querySelector("[name=message]");
 
 contactUs.addEventListener ("click", function(event) {
   event.preventDefault();
   contactUsPopup.classList.add("information-popup-show");
+  formName.focus();
 });
 closeInformation.addEventListener ("click", function(event) {
   event.preventDefault();
   contactUsPopup.classList.remove("information-popup-show");
+});
+form.addEventListener ("submit", function(event){
+  if (!formEmail.value || !formMessage.value || !formEmail.value) {
+    event.preventDefault();
+    alert("Заполните все поля");
+  };
+});
+
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+  if (contactUsPopup.classList.contains("information-popup-show")){
+    contactUsPopup.classList.remove("information-popup-show");
+  }  else if (cartPopup.classList.contains("cart-popup-show")) {
+      cartPopup.classList.remove("cart-popup-show");
+    } else if (mapPopup.classList.contains("map-popup-show")) {
+      mapPopup.classList.remove("map-popup-show")
+    };
+  };
 });
